d0b2403393a725f6647665f5cae27bc22bc85956
eli26m <veintiseisdemarzo@gmail.com>
2022-06-28 21:58:30 -0300

    primer commit

---
+++ BlogSpring/src/main/java/com/eli26javabootcamp/blogspring/BlogspringApplication.java
@@ -0,0 +1,13 @@
+package com.eli26javabootcamp.blogspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogspringApplication.class, args);
+	}
+
+}
---
+++ BlogSpring/src/main/java/com/eli26javabootcamp/blogspring/model/User.java
@@ -0,0 +1,5 @@
+package com.eli26javabootcamp.blogspring.model;
+
+public class User {
+
+}
