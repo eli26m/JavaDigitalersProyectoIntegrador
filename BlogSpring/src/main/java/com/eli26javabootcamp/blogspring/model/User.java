72f95d0982bfe136792d0b0256cd47fd37374cdf
eli26m <veintiseisdemarzo@gmail.com>
2022-07-21 21:58:54 -0300

    commit eclipse

--- BlogSpring/src/main/java/com/eli26javabootcamp/blogspring/model/User.java
+++ BlogSpring/src/main/java/com/eli26javabootcamp/blogspring/model/User.java
@@ -1,5 +1,44 @@
 package com.eli26javabootcamp.blogspring.model;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
+import org.springframework.data.annotation.Id;
+
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column
+	private String userNama;
+	@Column
+	private String password;
+	@Column
+	private String email;
+
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getUserNama() {
+	return userNama;
+}
+public void setUserNama(String userNama) {
+	this.userNama = userNama;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
 }
